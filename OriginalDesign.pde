d83dff65684871a25932b5668da4c0ac81bb00dc
unknown <kigil@697-334-eomer.sf.k12.internal>
2015-08-27 11:37:14 -0700

    night/day colors for ground

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,51 +1,90 @@
+
+
 void setup()
 {
-	size(600, 600);
+  size(600, 600);
+  
 }
-void draw()
-{
 
-  background(212, 224,155);
+
+
+
+void draw() {
+  
+  /*GROUND*/
+  /*background(backA,backB,backC);*/
   noStroke();
-  
-  int groundy;
-  groundy = 450;
-  //ground
-  fill(211,150,69); 
+  fill(groA,groB,groC);
   rect(0,450, 600, 350);
-  
+}
 
-cactus();
+int groA = 211;
+int groB = 150;
+int groC= 69;
+
+void mouseClicked() {
+  /*SKY COLOR*/
+
+  /*END SKY*/
+
+    /*GROUND COLOR*/
+
+    if (groA == 211) {
+      groA = 72;
+      } 
+    else {
+      groA = 211;
+    }
+
+   if (groB == 150) {
+      groB = 53;
+    } 
+    else {
+      groB = 150;
+      }
+
+  if (groC == 69) {
+    groC = 44;
+  } 
+  else {
+      groC = 69;
+  }
+
+  /*END GROUND*/
+
+  /*CACTUS COLOR*/
+
+  /*END CACTUS*/
 
 }
 
 
 
 void cactus(){
-	int cactusy;
+  int cactusy;
     int cactusx;
     cactusy = 450;
     cactusx = 60;
     noStroke();
 
-	fill(160, 178, 118);
-	rect(cactusx, cactusy, 50,-150); //body
-	rect(cactusx-41, cactusy-96,41,36); //left arm
-	rect(cactusx+41, cactusy-87, 54,33); //right arm
-	rect(cactusx+63, cactusy-118,32,43); //right branch
-	rect(cactusx-41, cactusy-118,30,32); //left branch
+  fill(160, 178, 118);
+  rect(cactusx, cactusy, 50,-150); //body
+  rect(cactusx-41, cactusy-96,41,36); //left arm
+  rect(cactusx+41, cactusy-87, 54,33); //right arm
+  rect(cactusx+63, cactusy-118,32,43); //right branch
+  rect(cactusx-41, cactusy-118,30,32); //left branch
 }
-void nightcactus(){
-	int cactusy;
+void nightCactus(){
+  int cactusy;
     int cactusx;
     cactusy = 450;
     cactusx = 60;
     noStroke();
 
-	fill(108, 117, 88);
-	rect(cactusx, cactusy, 50,-150); //body
-	rect(cactusx-41, cactusy-96,41,36); //left arm
-	rect(cactusx+41, cactusy-87, 54,33); //right arm
-	rect(cactusx+63, cactusy-118,32,43); //right branch
-	rect(cactusx-41, cactusy-118,30,32); //left branch
+  fill(108, 117, 88);
+  rect(cactusx, cactusy, 50,-150); //body
+  rect(cactusx-41, cactusy-96,41,36); //left arm
+  rect(cactusx+41, cactusy-87, 54,33); //right arm
+  rect(cactusx+63, cactusy-118,32,43); //right branch
+  rect(cactusx-41, cactusy-118,30,32); //left branch
 }
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,56 +14,98 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+
+
 public void setup()
 {
-	size(600, 600);
+  size(600, 600);
+  
 }
-public void draw()
-{
 
-  background(212, 224,155);
+
+
+
+public void draw() {
+  
+  /*GROUND*/
+  background(1,1,1);
   noStroke();
-  
-  int groundy;
-  groundy = 450;
-  //ground
-  fill(211,150,69); 
+  fill(groA,groB,groC);
   rect(0,450, 600, 350);
-  
+}
 
-cactus();
+// int groA = 211;
+// int groB = 150;
+// int groC= 69;
+
+public void mouseClicked() {
+  /*SKY COLOR*/
+
+  /*END SKY*/
+
+    /*GROUND COLOR*/
+    int groA = 211;
+	int groB = 150;
+	int groC= 69;
+
+    if (groA == 211) {
+      groA = 72;
+      } 
+    else {
+      groA = 211;
+    }
+
+   if (groB == 150) {
+      groB = 53;
+    } 
+    else {
+      groB = 150;
+      }
+
+  if (groC == 69) {
+    groC = 44;
+  } 
+  else {
+      groC = 69;
+  }
+
+  /*END GROUND*/
+
+  /*CACTUS COLOR*/
+
+  /*END CACTUS*/
 
 }
 
 
 
 public void cactus(){
-	int cactusy;
+  int cactusy;
     int cactusx;
     cactusy = 450;
     cactusx = 60;
     noStroke();
 
-	fill(160, 178, 118);
-	rect(cactusx, cactusy, 50,-150); //body
-	rect(cactusx-41, cactusy-96,41,36); //left arm
-	rect(cactusx+41, cactusy-87, 54,33); //right arm
-	rect(cactusx+63, cactusy-118,32,43); //right branch
-	rect(cactusx-41, cactusy-118,30,32); //left branch
+  fill(160, 178, 118);
+  rect(cactusx, cactusy, 50,-150); //body
+  rect(cactusx-41, cactusy-96,41,36); //left arm
+  rect(cactusx+41, cactusy-87, 54,33); //right arm
+  rect(cactusx+63, cactusy-118,32,43); //right branch
+  rect(cactusx-41, cactusy-118,30,32); //left branch
 }
-public void nightcactus(){
-	int cactusy;
+public void nightCactus(){
+  int cactusy;
     int cactusx;
     cactusy = 450;
     cactusx = 60;
     noStroke();
 
-	fill(108, 117, 88);
-	rect(cactusx, cactusy, 50,-150); //body
-	rect(cactusx-41, cactusy-96,41,36); //left arm
-	rect(cactusx+41, cactusy-87, 54,33); //right arm
-	rect(cactusx+63, cactusy-118,32,43); //right branch
-	rect(cactusx-41, cactusy-118,30,32); //left branch
+  fill(108, 117, 88);
+  rect(cactusx, cactusy, 50,-150); //body
+  rect(cactusx-41, cactusy-96,41,36); //left arm
+  rect(cactusx+41, cactusy-87, 54,33); //right arm
+  rect(cactusx+63, cactusy-118,32,43); //right branch
+  rect(cactusx-41, cactusy-118,30,32); //left branch
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
